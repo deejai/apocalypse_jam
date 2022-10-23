816c28b854d77a406923cfef563625636a60782d
= <=>
2022-10-23 17:17:44 -0600

    removed unused code

--- Main/Data/ActivatedAbilityDetails.gd
+++ Main/Data/ActivatedAbilityDetails.gd
@@ -2,15 +2,6 @@ extends Node
 
 class_name ActivatedAbilityDetails
 
-var key: String
-var icon: Sprite2D
-var cooldown: int
-var level: int
-var targeting_type: ActivatedAbility.TARGETING_TYPE
-var area_of_effect: int
-var effect: ArenaUnitEffect
-var range: int
-
 var data = {
 	"Mind Dart": {
 		"icon": load("res://Assets/PNG/bg.png"),
