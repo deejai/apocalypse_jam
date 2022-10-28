ebc9293f5affe93e4952e97653d31933108e5155
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-10-27 20:14:58 -0400

    added BALANCE battle

--- Main/Classes/Battle.gd
+++ Main/Classes/Battle.gd
@@ -24,12 +24,19 @@ func _init(level: int):
 					unit.speed *= 2
 					enemies.append({"unit": unit, "start_position": position})
 		TYPE.HEAL_ONE:
+			for c in range(randi_range(2,4)):
+				var position = Vector2(150 + c*150, 50 + 25 + 70)
+				var healer_unit = Unit.new(Unit.BASE.HEALER, level)
+				enemies.append({"unit": healer_unit, "start_position": position})
+				var strong_unit = Unit.new(Unit.BASE.SOLDIER_SPEAR, level)
+				strong_unit.hp *= 2
+				enemies.append({"unit": strong_unit, "start_position": Vector2(150 + c*150, 50 + 25 + 150)})
+		TYPE.BALANCED:
 			for c in range(randi_range(3,6)):
 				var position = Vector2(150 + c*150, 50 + 25 + 70)
-				var unit = Unit.new(Unit.BASE.HEALER, level)
-				enemies.append({"unit": unit, "start_position": position})
-
-			var beefy_soldier = Unit.new(Unit.BASE.SOLDIER_SPEAR, level)
-			beefy_soldier.attack_damage *= 2
-			beefy_soldier.hp *= 2
-			enemies.append({"unit": beefy_soldier, "start_position": Vector2(450, 50 + 25 + 150)})
+				var heal_unit = Unit.new(Unit.BASE.HEALER, level)
+				var ranged_unit = Unit.new(Unit.BASE.ARCHER, level)
+				var strong_unit = Unit.new(Unit.BASE.SOLDIER_SPEAR, level)
+				enemies.append({"unit": heal_unit, "start_position": position})
+				enemies.append({"unit": ranged_unit, "start_position": Vector2(150 + c*150, 50 + 25 + 120)})
+				enemies.append({"unit": strong_unit, "start_position": Vector2(150 + c*150, 50 + 25 + 180)})
